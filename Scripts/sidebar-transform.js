3df7ac204ed53995da1202ca174097bb07e657f2
Piyush Mudgal <mudgalpiyush22@gmail.com>
2023-09-03 23:20:54 +0530

    added expanded-sidebar

---
+++ Scripts/sidebar-transform.js
@@ -0,0 +1,25 @@
+function changeSidebar() {
+  const sidebarElement = document.querySelector('.sidebar');
+  if(sidebarElement.classList.value.includes('sidebar-normal')) {
+      sidebarElement.classList.remove('sidebar-normal');
+      sidebarElement.classList.add('sidebar-expanded');
+      document.querySelector('body').classList.add('body-expanded');
+      const sidebarIconElementList = document.querySelectorAll('.sidebar-icons');
+      sidebarIconElementList.forEach(element => {
+          element.classList.remove('sidebar-icons-container-normal');
+          element.classList.add('sidebar-icons-container-expanded');
+      });
+
+
+  } else {
+      console.log('hi');
+      sidebarElement.classList.remove('sidebar-expanded');
+      sidebarElement.classList.add('sidebar-normal');
+      document.querySelector('body').classList.remove('body-expanded');
+      const sidebarIconElementList = document.querySelectorAll('.sidebar-icons');
+      sidebarIconElementList.forEach(element => {
+          element.classList.add('sidebar-icons-container-normal');
+          element.classList.remove('sidebar-icons-container-expanded');
+      });
+  }
+}
